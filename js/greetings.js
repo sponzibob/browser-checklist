ca700d68e6f9841f77c47f15cbdde329710eb072
sponzibob <99729624+sponzibob@users.noreply.github.com>
2022-08-13 00:50:15 +0900

    modify greeting

--- js/greetings.js
+++ js/greetings.js
@@ -10,14 +10,37 @@ function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const typedUserName = loginInput.value;
+  paintGreeting(typedUserName);
   localStorage.setItem(USERNAME_KEY, typedUserName);
   //greeting.innerText = "Hello " + username; 이것은 `Hello ${username}`; 과 같은 결과를 나타낸다. python의 f{}와 같음.
-  paintGreeting(typedUserName);
 }
 
 function paintGreeting(username) {
+  const date = new Date();
+  const hours = date.getHours();
+  const savedUsername = localStorage.getItem(USERNAME_KEY);
   greeting.classList.remove(HIDDEN_CLASSNAME);
-  greeting.innerText = `Hello ${username}`;
+  if (savedUsername === null) {
+    if (hours <= 6) {
+      greeting.innerText = `Good night, ${username}`;
+    } else if (hours <= 12) {
+      greeting.innerText = `Good morning, ${username}`;
+    } else if (hours <= 18) {
+      greeting.innerText = `Good afternoon, ${username}`;
+    } else {
+      greeting.innerText = `Good evening, ${username}`;
+    }
+  } else {
+    if (hours <= 6) {
+      greeting.innerText = `Good night, ${savedUsername}`;
+    } else if (hours <= 12) {
+      greeting.innerText = `Good morning, ${savedUsername}`;
+    } else if (hours <= 18) {
+      greeting.innerText = `Good afternoon, ${savedUsername}`;
+    } else {
+      greeting.innerText = `Good evening, ${savedUsername}`;
+    }
+  }
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -28,3 +51,5 @@ if (savedUsername === null) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   paintGreeting(savedUsername);
 }
+
+setInterval(paintGreeting, 60000);
